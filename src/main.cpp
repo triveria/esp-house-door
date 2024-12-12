b8263815df126f2eb46357edfbfc048bff4a66fc
Michael Wagner <michael.wagner.31337@gmail.com>
2024-12-12 10:01:06 +0000

    Serve dummy website

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,55 @@
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+#include <WiFi.h>
 
 #include "private_wifi_credentials.hpp"
+
+AsyncWebServer server(80);
 
 void setup()
 {
     Serial.begin(115200);
     delay(10);
     Serial.println();
+
+    // Initialize SPIFFS
+    if (!SPIFFS.begin(true)) {
+        Serial.println("An error has occurred while mounting SPIFFS");
+        return;
+    }
+
+    // Connect to WiFi
+    Serial.print("Connecting to WiFi...");
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("Connected to WiFi!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    // Serve the HTML file
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+
+    // Handle form submission
+    server.on("/submit", HTTP_POST, [](AsyncWebServerRequest *request) {
+        if (request->hasParam("foo", true)) {
+            String fooValue = request->getParam("foo", true)->value();
+            Serial.println("Value of foo: " + fooValue);
+        }
+        request->send(200, "text/plain", "Value received");
+    });
+
+    server.begin();
 }
 
 void loop()
 {
-
-    Serial.println("Hello World!");
-    delay(10);
+    // Nothing to do here
 }
