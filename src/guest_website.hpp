d2b81db2cc530d8f146248548e3af2bf038dfcd5
Michael Wagner <michael.wagner.31337@gmail.com>
2024-12-12 15:18:16 +0000

    Refactor cpp code for website

---
+++ src/guest_website.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <ESPAsyncWebServer.h>
+
+void setupServer(AsyncWebServer &server);
--- src/main.cpp
+++ src/main.cpp
@@ -3,75 +3,10 @@
 #include <SPIFFS.h>
 #include <WiFi.h>
 
+#include "guest_website.hpp"
 #include "private_wifi_credentials.hpp"
 
 AsyncWebServer server(80);
-
-void handleRootRequest(AsyncWebServerRequest *request)
-{
-    Serial.println("Handling root request");
-    request->send(SPIFFS, "/index.html", "text/html");
-}
-
-void handleFormSubmission(AsyncWebServerRequest *request)
-{
-    Serial.println("Handling form submission");
-    if (request->hasParam("foo", true)) {
-        String fooValue = request->getParam("foo", true)->value();
-        Serial.println("Value of foo: " + fooValue);
-    }
-    request->send(200, "text/plain", "Value received");
-}
-
-void handleSaveGuestsRequest(AsyncWebServerRequest *request)
-{
-    // Leave empty; actual handling occurs in the body handler
-}
-
-void handleSaveGuestsBody(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
-{
-    if (index == 0) {
-        // First chunk of data
-        request->_tempObject = new String();
-    }
-    String *body = (String *)request->_tempObject;
-    body->concat((char *)data, len);
-
-    if ((index + len) == total) {
-        // All data received
-        Serial.println("Handling save guests request");
-        Serial.println("Received body: ");
-        Serial.println(*body);
-
-        // Write the body to the file
-        File file = SPIFFS.open("/guests.json", FILE_WRITE);
-        if (!file) {
-            Serial.println("Failed to open file for writing");
-            request->send(500, "text/plain", "Failed to open file for writing");
-            delete body;
-            request->_tempObject = NULL;
-            return;
-        }
-
-        size_t bytesWritten = file.print(*body);
-        file.close();
-
-        Serial.print("Bytes written to file: ");
-        Serial.println(bytesWritten);
-
-        if (bytesWritten == 0) {
-            Serial.println("Failed to write data to file");
-            request->send(500, "text/plain", "Failed to write data to file");
-        } else {
-            Serial.println("Data written successfully");
-            request->send(200, "application/json", "{\"status\":\"success\"}");
-        }
-
-        // Clean up
-        delete body;
-        request->_tempObject = NULL;
-    }
-}
 
 void setup()
 {
@@ -99,20 +34,8 @@ void setup()
     Serial.print("IP Address: http://");
     Serial.println(WiFi.localIP());
 
-    // Serve the HTML file
-    server.on("/", HTTP_GET, handleRootRequest);
-
-    // Handle form submission
-    server.on("/submit", HTTP_POST, handleFormSubmission);
-
-    // Serve static files
-    server.serveStatic("/", SPIFFS, "/");
-
-    // Handle saving guests
-    server.on("/save-guests", HTTP_POST, handleSaveGuestsRequest, NULL, handleSaveGuestsBody);
-
-    server.begin();
-    Serial.println("Server started");
+    // Setup the server
+    setupServer(server);
 }
 
 void loop()
